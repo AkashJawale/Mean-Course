d1098e39a6e8357826a5453d65833d69b15991c6
AkashJawale <akashjawale131@gmail.com>
2018-10-25 19:14:46 +0530

    Deleting documents and updating frontend after after deleting

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Subject } from "rxjs";
+import { map } from 'rxjs/operators';
 
 import { Post } from "./post.model";
 
@@ -13,11 +14,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        "http://localhost:3000/api/posts"
+      .get<{ message: string; posts: any }>(
+        'http://localhost:3000/api/posts'
       )
-      .subscribe(postData => {
-        this.posts = postData.posts;
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return{
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+    }))
+      .subscribe(transformedPost => {
+        this.posts = transformedPost;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -36,4 +46,15 @@ export class PostsService {
         this.postsUpdated.next([...this.posts]);
       });
   }
+
+
+deletePost(postId: string){
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+       const updatedPost = this.posts.filter(post => post.id !== postId);
+       this.posts = updatedPost;
+     this.postsUpdated.next([...this.posts]);
+      });
+}
+
 }
