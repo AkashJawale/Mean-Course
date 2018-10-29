9232a1a81c42f22b4bc23b47a8d848a463bdba2a
AkashJawale <akashjawale131@gmail.com>
2018-10-29 18:01:15 +0530

    Use Mime-Type Calidator and Finishing the Image Validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, ParamMap } from "@angular/router";
 
 import { PostsService } from "../posts.service";
 import { Post } from "../post.model";
+import { mimeType} from './mime-type-validator';
 
 @Component({
   selector: "app-post-create",
@@ -34,7 +35,9 @@ export class PostCreateComponent implements OnInit {
       }),
       content: new FormControl(null, {
         validators: [Validators.required] }),
-        image: new FormControl(null, {validators: [Validators.required]})
+        image: new FormControl(null, {validators: [Validators.required],
+        asyncValidators: [mimeType]
+        })
     });
 
 
