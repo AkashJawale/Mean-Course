1b9494ac76d9778028de195ad01e2b3a68232890
AkashJawale <akashjawale131@gmail.com>
2018-11-01 02:23:41 +0530

    Adding Middleware to Protect Routes and adding token to Authenticate Requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,8 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { AuthData } from "./auth-data.model";
 
 @Injectable ({ providedIn: "root"})
-export class AuthService{
-  constructor(private  http: HttpClient){}
+export class AuthService {
+  private token: string;
+
+  constructor(private  http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password };
@@ -16,10 +22,11 @@ export class AuthService{
   }
 login(email: string, password: string ) {
     const authData: AuthData = {email: email, password: password};
-    this.http.post("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
       .subscribe(responce => {
-        console.log(responce);
-      });
+      const token = responce.token;
+      this.token = token;
+      })
    }
 
   }
