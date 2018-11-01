85196aa2d8cf881f15df0d1317d98062df8e8cfb
AkashJawale <akashjawale131@gmail.com>
2018-11-01 15:39:37 +0530

    Improving UI header and UI message to reflect Authentication Status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import {Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 import { AuthData } from "./auth-data.model";
 
 @Injectable ({ providedIn: "root"})
 export class AuthService {
+  private  isAuthenticated = false;
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private  http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string){
@@ -26,6 +37,11 @@ login(email: string, password: string ) {
       .subscribe(responce => {
       const token = responce.token;
       this.token = token;
+      if (token) {
+        this.isAuthenticated = true;
+
+        this.authStatusListener.next(true);
+      }
       })
    }
 
